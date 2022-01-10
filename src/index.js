7f7b061a63a23a86ed9eac0a1c6d544266d2d149
Mehmet <mehmetk6158@gmail.com>
2022-01-09 20:09:44 -0500

    add comment component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import faker from 'faker';
 const App = () => {
   return (
     <div className="ui contanier comments">
+      
+      <div classNAme="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.image()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+             Today at 6:00PM
+            </span>
+          </div>
+          <div className="text">
+            Nice blog post!
+          </div>
+        </div>
+      </div>
       <div classNAme="comment">
         <a href="/" className="avatar">
           <img alt="avatar" src={faker.image.image()}/>
