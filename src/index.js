d98198fe79d3cf73b8c0947c43ecc443022ffef0
Mehmet <mehmetk6158@gmail.com>
2022-01-10 10:45:34 -0500

    pass image prop

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui contanier comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blogpost"/>
-      <CommentDetail author="Alex" timeAgo="Today at 1:45PM" content="I like the subject"/>
-      <CommentDetail author="Mehmet" timeAgo="Yesterday at 11:45AM" content="I like playing soccer"/>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blogpost" avatar={faker.image.image()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 1:45PM" content="I like the subject" avatar={faker.image.image()}/>
+      <CommentDetail author="Mehmet" timeAgo="Yesterday at 11:45AM" content="I like playing soccer" avatar={faker.image.image()}/>
     </div>
   );
 };
