1c392a0b6a7d9aebec49cb321a0444291632c671
Mehmet <mehmetk6158@gmail.com>
2022-01-09 20:14:19 -0500

    call comment detail component

--- src/index.js
+++ src/index.js
@@ -6,43 +6,7 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui contanier comments">
-      
-      <div classNAme="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">
-             Today at 6:00PM
-            </span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
-      <div classNAme="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">
-             Today at 6:00PM
-            </span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
+      <CommentDetail/>
     </div>
   );
 };
