1f4b4179011b1b87b26c2548ab8ca455194a38f2
Mehmet <mehmetk6158@gmail.com>
2022-01-10 11:18:55 -0500

    create card for blog post

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './CardApproval';
 
 const App = () => {
   return (
     <div className="ui contanier comments">
+      <ApprovalCard/>
       <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blogpost" avatar={faker.image.image()}/>
       <CommentDetail author="Alex" timeAgo="Today at 1:45PM" content="I like the subject" avatar={faker.image.image()}/>
       <CommentDetail author="Mehmet" timeAgo="Yesterday at 11:45AM" content="I like playing soccer" avatar={faker.image.image()}/>
